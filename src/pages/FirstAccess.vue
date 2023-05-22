26b3b9f638a622f8568937d08d70c11f7a5e0e91
best <cleargoal01@gmail.com>
2023-05-22 23:18:50 +0300

    fix mark-up

--- src/pages/FirstAccess.vue
+++ src/pages/FirstAccess.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="surface-0 flex align-items-center justify-center min-w-screen overflow-hidden mt-12">
+    <div class="card surface-0 flex align-items-center justify-content-center max-w-full h-min overflow-hidden mt-12">
         <div
-            class="grid p-2 lg:p-0 justify-center"
-            style="min-width:80%"
+            class="grid p-2 lg:p-0 justify-content-center"
         >
-            <div class="col-12 mt-5 xl:mt-0 flex justify-center">
+            <div class="col-12 mt-5 xl:mt-0 flex justify-content-center">
                 <div class="min-w-min flex mb-5">
                     <img
                         src="/images/whole-europe.svg"
@@ -16,16 +15,16 @@
                 </div>
             </div>
             <div
-                class="col-12 xl:col-6 justify-center"
+                class="col-12 justify-center"
                 style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, rgba(247, 149, 48, 0.4) 10%, rgba(247, 149, 48, 0) 30%);"
             >
                 <div
-                    class="w-full m-0 py-7 px-4 flex"
+                    class="w-full m-0 py-7 px-4 flex justify-content-center"
                     style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));"
                 >
-                    <div class="grid flex flex-column justify-center py-8 px-16">
-                        <div class="flex flex-column justify-center">
-                            <h1 class="text-900 font-bold text-4xl lg:text-5xl mb-2">
+                    <div class="flex flex-column justify-content-center py-8 px-16">
+                        <div class="flex flex-column justify-content-center mb-4">
+                            <h1 class="text-600 font-bold text-4xl lg:text-5xl mb-2">
                                 Access Allowed. Welcome!
                             </h1>
                             <span class="text-600 text-center">You have any necessary permissions.</span>
@@ -34,7 +33,7 @@
                             <img
                                 src="/images/nature4.jpg"
                                 alt="Access Allowed. Welcome!"
-                                width="100%"
+                                width="500"
                             >
                         </div>
                         <div class="flex mt-4 justify-center">
@@ -54,11 +53,6 @@
     justify-content: center;
     justify-self: center;
 }
-
-.flex-column {
-    flex-direction: column;
-}
-
 
 .p-8 {
     padding: 2rem;
