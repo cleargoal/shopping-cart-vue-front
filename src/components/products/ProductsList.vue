75a174c6bf8139ea7a5771b404e0fdef00d67f44
best <cleargoal01@gmail.com>
2023-05-24 21:08:54 +0300

    add component ProductsList.vue

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -32,44 +32,9 @@
                         </div>
                     </template>
                     <template #list="slotProps">
-                        <div class="col-12">
-                            <div class="flex flex-column md:flex-row align-items-center p-3 w-full">
-                                <img
-                                    :src="'images/product/' + slotProps.data.image"
-                                    :alt="slotProps.data.name"
-                                    class="my-4 md:my-0 w-9 md:w-10rem shadow-2 mr-5"
-                                >
-                                <div class="flex-1 text-center md:text-left">
-                                    <div class="font-bold text-2xl">
-                                        {{ slotProps.data.name }}
-                                    </div>
-                                    <div class="mb-3">
-                                        {{ slotProps.data.description }}
-                                    </div>
-                                    <RatingMark
-                                        :model-value="slotProps.data.rating"
-                                        :readonly="true"
-                                        :cancel="false"
-                                        class="mb-2"
-                                    />
-                                    <div class="flex align-items-center">
-                                        <i class="pi pi-tag mr-2" />
-                                        <span class="font-semibold">{{ slotProps.data.category }}</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-row md:flex-column justify-content-between w-full md:w-auto align-items-center md:align-items-end mt-5 md:mt-0">
-                                    <span class="text-2xl font-semibold mb-2 align-self-center md:align-self-end">${{ normalPrice(slotProps.data.price) }}</span>
-                                    <ButtonDefault
-                                        icon="pi pi-shopping-cart"
-                                        label="Add to Cart"
-                                        :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
-                                        class="mb-2"
-                                        @click="addToCar(slotProps.data)"
-                                    />
-                                    <span :class="'product-badge status-'+slotProps.data.inventoryStatus.toLowerCase()">{{ slotProps.data.inventoryStatus }}</span>
-                                </div>
-                            </div>
-                        </div>
+                        <product-card-list
+                            :data="slotProps.data"
+                        />
                     </template>
 
                     <template #grid="slotProps">
@@ -89,9 +54,10 @@
     import ApiService from "../../service/ApiService";
     import { mapActions } from 'vuex';
     import ProductCardGrid from "./ProductCardGrid.vue";
+    import ProductCardList from "./ProductCardList.vue";
 
     export default {
-        components: {ProductCardGrid},
+        components: {ProductCardGrid, ProductCardList},
         data() {
             return {
                 categoryHeader: '',
@@ -121,6 +87,7 @@
             },
         },
         created() {
+            this.setShowPreloader();
             this.ApiService = new ApiService();
             this.getProductsData();
         },
@@ -150,7 +117,7 @@
                 await this.setHidePreloader();
             },
             ...mapActions('cartModule', ['addToCart']),
-            ...mapActions('commonModule', ['setHidePreloader']),
+            ...mapActions('commonModule', ['setHidePreloader', 'setShowPreloader']),
         },
     }
 </script>
