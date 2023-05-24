91c9e82ee61db7aca1ca9a883fdccf7f306514d3
best <cleargoal01@gmail.com>
2023-05-24 21:06:55 +0300

    add to 'utils' the function 'normalPrice'

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,6 +5,10 @@ export function updateLocalStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value))
 }
 
+export function normalPrice(val) {
+    return (val / 100).toFixed(2);
+}
+
 export default defineComponent({
     components: {ShoppingCart}
 })
