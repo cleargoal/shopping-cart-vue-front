9f96fe00deda6638e38d922bd296d0ddfdab4940
best <cleargoal01@gmail.com>
2023-06-18 10:42:25 +0300

    add 'scrollBehavior' to router for scrolling page to top when category changed

--- src/router.js
+++ src/router.js
@@ -51,6 +51,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        // always scroll to top
+        return { top: 0 }
+    },
 });
 
 export default router;
